b1e3e421666de9f71c978679e26f4f8cb7034ef0
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-10 03:04:14 +0300

    add Example005 Conditions If-Else

---
+++ Example005_Conditionals_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анна") // для того чтобы ToLower работала, имя нужно записать с маленькой буквы
+{
+    Console.Write("Как я по вам скучала, Анна!");
+}
+else
+{
+    Console.Write("Привет, " + username);
+}
