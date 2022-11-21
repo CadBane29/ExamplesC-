5948b14703bfcd9383b7e03f364e42867b51ebe2
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-21 22:23:38 +0300

    Method Array Example012.02

--- Example012.01_Methods/Program.cs
+++ Example012.01_Methods/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Create a calculator.
 
-// for (int i = 1; i <= 10; i++)
-// {
-//     for (int j = 1; j <= 10; j++)
-//     {
-//         Console.WriteLine($"{i} x {j} = {i * j}");
-//     }
-//     Console.WriteLine();
-// }
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
 
 // ==== Работа с текстом 
 // Дан текст. В тексте нужно все пробелы заменить черточками, - что значит "дан текст" и что заначит "черточками"?
@@ -16,32 +16,32 @@
 
 // Ясна ли задача?
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели бы вас послали вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля. "
-            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
 
-// string s = "qwerty"
-//             012345
-// s[3] - r  // мы можем обратиться к какому-то символу строки [] 
+// // string s = "qwerty"
+// //             012345
+// // s[3] - r  // мы можем обратиться к какому-то символу строки [] 
 
-string Replace(string text, char oldValue, char newValue)
-{
-    string result = String.Empty;  // заводим пустую строку
-    int length = text.Length; // для получения длины нашей строки
-    for (int i = 0; i < length; i++)
-    {
-        if(text[i] == oldValue) result = result + $"{newValue}";// если текущий символ совпал с тем, который мы хотим заменить
-        else result = result + $"{text[i]}"; // если совпадений не найдено, то наш результат нужно добавить символ, который и был
-    }
-    return result;
-}
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;  // заводим пустую строку
+//     int length = text.Length; // для получения длины нашей строки
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";// если текущий символ совпал с тем, который мы хотим заменить
+//         else result = result + $"{text[i]}"; // если совпадений не найдено, то наш результат нужно добавить символ, который и был
+//     }
+//     return result;
+// }
 
-string newText = Replace(text, ' ', '-');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'C', 'c');
-Console.WriteLine(newText);
+// string newText = Replace(text, ' ', '-');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'C', 'c');
+// Console.WriteLine(newText);
---
+++ Example012.02_MethodArray/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)    // метод вывода данного массива на экран
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // метод сортировки нашего массива
+{
+    for (int i = 0; i < array.Length - 1; i++) // блок нахождения минимальной позиции
+    {
+        int minPosition = i;
+        // ниже блок, который ищет максимальный элемент
+        for (int j = i + 1; j < array.Length; j++) // начинаем прокручивать массив уже со следующей позиции
+        {   //если элемент array[i] меньше пердполагаемого элемента на минимальной позиции, то эту позицию нужно сохранить
+            if (array[j] < array[minPosition]) minPosition = j; // если поменять знак < на >, то массив будет от большего
+                                                                                                            // к меньшему
+        }
+        int temporary = array[i]; // меняем минимальную позицию с той позицией на которой находимся
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
